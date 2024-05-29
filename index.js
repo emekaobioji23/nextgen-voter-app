2a6f336530b127d1c236e78229fd198735452733
emekaobioji23 <emekaobioji23@gmail.com>
2024-05-29 15:06:44 +0100

    simple change to create a commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { PORT, DB_URL } = process.env;
 mongoose.set('strictQuery', false);
 mongoose
   .connect(DB_URL)
-  .then(() => console.log("db connected"))
+  .then(() => console.log("db has connected"))
   .catch((err) => console.log(err));
 
 app.listen(PORT || 4000, () => console.log("server is running at",PORT));
