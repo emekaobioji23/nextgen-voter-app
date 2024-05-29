cec7b4e01dd4f72a1b14511bb3cf575f8ea22a44
emekaobioji23 <emekaobioji23@gmail.com>
2024-05-29 14:49:56 +0100

    swapped index with server and renamed server to index in package.json

--- index.js
+++ index.js
@@ -1,47 +1,14 @@
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+dotenv.config({ path: "./config.env" });
+const app = require("./server");
 
-const express = require("express");
-const morgan = require("morgan");
-const fs = require("fs");
-const path = require("path");
-const cors = require("cors");
-const adminRoute = require("./routes/admin-route");
-const voterRoute = require("./routes/voter-route");
-const ErrorHandler = require("./controllers/error-controller");
-const ErrorObject = require("./utils/error");
-const { PORT } = process.env;
+const { PORT, DB_URL } = process.env;
 
-const app = express();
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "access.log"),
-  { flags: "a" }
-);
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
 
-
-// Middlewares
-
-// body parser
-app.use(express.json());
-
-// Cors
-app.use(cors());
-
-// Using Static files
-app.use(express.static(`${__dirname}/public`));
-
-// setup the logger
-app.use(morgan("combined", { stream: accessLogStream }));
-
-// Route
-
-app.use("/api/v1/admins", adminRoute);
-app.use("/api/v1", voterRoute);
-
-app.all("*", (req, res, next) => {
-  const err = new ErrorObject(
-    `${req.protocol}://${req.get("host")}${req.url} not found`, 404);
-  next(err);
-});
-
-// Error Handling
-app.use(ErrorHandler);
-module.exports = app;
+app.listen(PORT || 4000, () => console.log("server is running at",PORT));
